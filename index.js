0e7bc65b60240139d9f1f1092d1676321976c473
josefa felix <josefafelixfefe@gmail.com>
2021-10-19 11:49:04 +0100

    lj

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
 const express=require("express")
 const app=express();
+const xlsxFile=require("read-excel-file/node");
+
+
+
+
+
+app.get("/excel",function(req,res){
+
+
+    xlsxFile("./report.xlsx",{getSheets:true}).then((sheets)=>{
+        sheets.forEach((obj)=>{
+          console.log(obj.name)
+          
+        })
+    })
+
+ 
+})
+
+
+
+
+app.get("/paginas/:valor",function(req,res){
+    
+    xlsxFile("./report.xlsx",{sheet:req.params.valor}).then((rows)=>{
+        for (i in rows){
+            for(j in rows[i]){
+                console.log(rows[i][j]);
+            }
+        }
+    
+    })
+ 
+ })
+
+ 
+
 
 
 
